28e4ca34af6001209462e8c96417283a200a59d7
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2019-09-28 20:08:00 +0900

    top#translate

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -3,5 +3,8 @@ class TopController < ApplicationController
   end
 
   def translate
+    @source = params[:source]
+    @result = @source.upcase # todo: suri_langを使って@sourceを変換する
+    render :show
   end
 end
